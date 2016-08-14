1edcdcd74c60750005300c717814fe7f60c23c5e
Rahul Syamlal <rsyamlal@gmail.com>
2016-08-14 17:37:36 -0500

    added polish notation evaluation & simple compile script

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+ cc -std=c99 -Wall prompt.c mpc/mpc.c -ledit -lm -o rah 
--- prompt.c
+++ prompt.c
@@ -18,6 +18,38 @@
 
 #include <editline/readline.h>
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */ 
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;  
+}
+
 int main(int argc, char **argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number   = mpc_new("number");
@@ -51,9 +83,9 @@ int main(int argc, char **argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Rah, &r)) {
-          /* On Success Print the AST */
-          mpc_ast_print(r.output);
-          mpc_ast_delete(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
         } else {
           /* Otherwise Print the Error */
           mpc_err_print(r.error);
